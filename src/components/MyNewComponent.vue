9f2b95cba42940bd6dd0672e462dd94649ab5dfc
Julian <jallende@bu.edu>
2024-04-29 19:03:07 -0700

    pushing changes

---
+++ src/components/MyNewComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>MyNewComponent</h1>
+</template>
+
+<script>
+  export default {
+    name: 'MyNewComponent'
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
